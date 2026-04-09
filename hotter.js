3f7f4cefc15b66ef41284f8db6012d440112b8b9
esam <emmanueldaud358@gmail.com>
2026-04-10 00:20:24 +0300

    third commit

--- hotter.js
+++ hotter.js
@@ -1,4 +1,13 @@
 const method = ['asha' , 'amina' , 'rose'];
 
-
+// trying to understand about http in javascript
+name.forEach(name =>{
+    fetch("..............." , {
+        method : 'post',
+        Headers : {
+            name : 'ashura',
+            age : 23
+        },
+    })
+});
 console.log('nothig here still working on this ');
