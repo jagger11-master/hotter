7bbce9c235b4cd93f078448f6007eb4bb16f4c56
esam <emmanueldaud358@gmail.com>
2026-04-12 23:52:25 +0300

    nineth commit

--- laugh.js
+++ laugh.js
@@ -1,4 +1,12 @@
-function myFunction(numb1 , numb2) {
-    
-    return a + b;
-}
+const person =  {
+    startName: "kelvin",
+    middleName: "Crey",
+    lastName: "emmaneul",
+    fullName: function() {
+        console.log(fullnName);
+        return this.startName + "" + this.middleName + ""  + this.lastName;
+
+    }
+};
+
+person.fullName ();
