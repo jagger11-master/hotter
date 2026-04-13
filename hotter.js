62473cc08bceeb0ac3faddad1a43fe59e792208c
esam <emmanueldaud358@gmail.com>
2026-04-13 09:08:35 +0300

    commit

--- hotter.js
+++ hotter.js
@@ -1,48 +1,10 @@
-const method = ['asha' , 'amina' , 'rose'];
-/*
-// trying to understand about http in javascript
-name.forEach(name =>{
-    fetch("..............." , {
-        method : 'post',
-        Headers : {
-            name : 'ashura',
-            age : 23
-        },
-    })
+const camerafeed = document.getElementById("cameraFeed")
+
+navigator.mediaDevices.getUserMedia((video:true));
+
+.then(stream => {
+    cameraFeed.srcObject = stream;
+})
+.catch(error => {
+    console.errorr("error accessing camera",error);
 });
-console.log('nothig here still working on this ');
-
-// window in javascript 
-
-
-setTimeout(greet,3000);
-
-function greet() {
-    alert("hello there");
-
-    console.log(greet);
-
-}
-    
-
-
-const number = [2 , 4 ,5 , 6];
-const number2 = number.map(meFunc);
-
- 
-    function meFunc(number2){
-         return number2 * 2; 
-         
- }
- console.log(number2);
- 
-*/
-
-const person = {
-    firstName: "emmanuel",
-    lastName: "hamis",
-    finalName: function () {
-        console.log(finalName);
-        return this.firstName + "" + this.lastName;
-    }
-};
