ecd0ee80e7f71b5b155867870fb4ba235c4fa69d
esam <emmanueldaud358@gmail.com>
2026-04-28 16:49:43 +0300

    commit

---
+++ lcd.c
@@ -0,0 +1,24 @@
+int trig = 6;
+int echo = 7;
+void setup() {
+  // put your set p code here, to run once:
+pinMode(trig,OUTPUT);
+pinMode(echo,INPUT);
+Serial.begin(9600);
+}
+
+void loop(){
+  digitalWrite(trig,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig,LOW);
+  int duration = pulseIn(echo,HIGH);
+  int distance = 0.034 * duration/2;
+  Serial.print("object distance");
+  Serial.print(distance);
+  Serial.print("");
+  Serial.print("cm");
+  Serial.print("\n");
+  delay(10000);
+}
